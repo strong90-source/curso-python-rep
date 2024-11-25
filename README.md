b2e088ba4799b9a0d9cd7929083c6082cbc53ba1
Ricardo Martins <martins.ricardo1@hotmail.com>
2024-11-25 09:31:46 +0000

    Adding a few more lessons

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # curso-python-rep
-Python Course
+Python Course from Udemy -> https://www.udemy.com/course/python-3-do-zero-ao-avancado/learn/lecture/34566546#overview
---
+++ aula17.py
@@ -0,0 +1,17 @@
+# if / elif ....../ else
+# se / se nao se....../ senao
+
+condicao = True
+condicao1 = False
+condicao2 = False 
+condicao3 = False
+condicao4 = False
+
+
+if condicao: 
+    print('Este e o codigo do if')
+elif condicao == False:
+    print('Ze manel')
+else:
+    print('Ze Maria')
+
