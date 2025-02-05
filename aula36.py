37986c2822ab9f99e6c26f43f233c49c31f49c66
Ricardo Martins <martins.ricardo1@hotmail.com>
2025-02-05 22:23:30 +0000

    While with = += -= *= /= //= **= %=

---
+++ aula36.py
@@ -0,0 +1,16 @@
+"""
+Operadores de atribuicao
+= += -= *= /= //= **= %=
+"""
+
+contador = 10
+
+# while contador <= 10:
+#     print(contador)
+#     contador = contador + 1
+
+
+# print('Acabou')
+
+contador *= '2'
+print(contador)
