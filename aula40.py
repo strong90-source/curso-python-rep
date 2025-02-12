5871de733b0a077a284f7aa69657e6a22949b1c3
Ricardo Martins <martins.ricardo1@hotmail.com>
2025-02-12 22:19:41 +0000

    while calculator

---
+++ aula40.py
@@ -0,0 +1,49 @@
+""" Calculadora com while"""
+
+while True:
+    numero_1 = input('Digite um numero: ')
+    numero_2 = input('Digite outro numero: ')
+    operador = input('Digite um operador: ')
+
+    numeros_validos = None
+    numero_1_float = 0
+    numero_2_float = 0
+
+    try:
+        numero_1_float = float(numero_1)
+        numero_2_float = float(numero_2)
+        numeros_validos = True
+
+    except:
+        numeros_validos = None
+    
+    if numeros_validos is None:
+        print('Um ou ambos os  numeros que digitou sao invalidos')
+        continue
+
+    operadores_permitidos = '+-/*'
+
+    if operador not in operadores_permitidos:
+        print('Operador invalido!!! ')
+    
+    if len(operador) > 1:
+        print('Digite apenas um operador. ')
+
+    print('Verificando a sua conta. Confira o resultado abaixo!!!')
+
+    if operador == '+':
+        print(f'{numero_1_float} + {numero_2_float}=', numero_1_float + numero_2_float)
+    elif operador == '-':
+        print(f'{numero_1_float} - {numero_2_float}=', numero_1_float - numero_2_float)
+    elif operador == '/':
+        print(f'{numero_1_float} / {numero_2_float}=', numero_1_float / numero_2_float)
+    elif operador == '*':
+        print(f'{numero_1_float} * {numero_2_float}=', numero_1_float * numero_2_float)
+    else:
+        print('Nunca deveria chegar aqui.')
+
+    sair= input('Quer sair?? [s]sim: ' ).lower().startswith('s')
+    
+    if sair is True:
+        break
+
